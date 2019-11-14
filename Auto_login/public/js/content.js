285498bf2ad5458fd08b079e95ccd0935078d1ed
Hoang Van Huy <huynoone@gmail.com>
2019-11-14 18:42:19 +0700

    Update content.js

--- Auto_login/public/js/content.js
+++ Auto_login/public/js/content.js
@@ -1,17 +1,4 @@
-$(document).ready(function() {
-	chrome.storage.sync.get(["auto-login"], function(checkbox) {
-		if (checkbox["auto-login"]) {
-			login();
-		}
-	});
-	chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            if( request.message === "start" ) {
-                login();
-            }
-        }
-    );
-});
+
 function login() {
 	chrome.storage.sync.get(["userselected", "passselected"], function(result) {
 		$('input[type=text]').val(result["userselected"]);
