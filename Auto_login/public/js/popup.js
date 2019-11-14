7d346841e8b46ab6021c3ecafb9ddb55bc1d9eef
Hoang Van Huy <huynoone@gmail.com>
2019-11-14 19:44:14 +0700

    Update popup.js

--- Auto_login/public/js/popup.js
+++ Auto_login/public/js/popup.js
@@ -160,7 +160,29 @@ $(document).ready(function () {
   	});
 }); //end document ready
 
-
+function loadCurrentCookie() {
+    chrome.tabs.getSelected(null, function (tab) { //lấy cửa sổ làm việc hiện tại
+		var currentUrl=tab.url;
+		//add địa chỉ url đã lọc
+		$('#CurrentCookieUrl').val(extractHostname(currentUrl));
+		
+    });
+}
+function extractHostname(url) {
+	var hostname;
+	//lọc url của web
+	if (url.indexOf("://") > -1) {
+		hostname = url.split('/')[2];
+	}
+	else {
+		hostname = url.split('/')[0];
+	}
+	//tìm và xóa địa chỉ port
+	hostname = hostname.split(':')[0];
+	//tìm và xóa "?"
+	hostname = hostname.split('?')[0];
+	return hostname;
+}
 //gọi chức năng load loadCurrentCookie()
 loadCurrentCookie();
 
