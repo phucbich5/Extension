7ef5522c6c60604c3a60561bcf9e4ccb21ef3579
Hoang Van Huy <huynoone@gmail.com>
2020-11-11 14:12:06 +0700

    Update content.js

--- Auto_login/public/js/content.js
+++ Auto_login/public/js/content.js
@@ -1,4 +1,19 @@
-
+$(document).ready(function(){
+	chrome.storage.sync.get(["auto-login"], function(result) {
+		if (result["auto-login"]) {
+			login();
+		}
+	});
+	
+	chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if( request.message === "start" ) {
+                login();
+            }
+        }
+    );
+       
+});
 function login() {
 	chrome.storage.sync.get(["userselected", "passselected"], function(result) {
 		$('input[type=text]').val(result["userselected"]);
